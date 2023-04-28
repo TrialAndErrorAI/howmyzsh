349e941ee46b09d899b741b7e4b85a6ddbad10b6
Sid Sarasvati <2634859+sidsarasvati@users.noreply.github.com>
2023-04-28 13:47:13 -0400

    source is bash not shuse . instead

--- run_app.sh
+++ run_app.sh
@@ -5,7 +5,7 @@ if [ ! -d ".venv" ]; then
     echo "Creating virtual environment..."
     python3 -m venv .venv
 fi
-source ./.venv/bin/activate
+. ./.venv/bin/activate
 pip install --no-cache-dir -r requirements.txt
 
 streamlit run app.py --server.port=8080
