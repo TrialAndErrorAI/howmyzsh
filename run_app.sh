4690fdcf911f3dc53a5087fab9b7883a2b262a85
Sid Sarasvati <2634859+sidsarasvati@users.noreply.github.com>
2023-04-28 13:22:58 -0400

    create python venv before install duh

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-
+set -e
+# create virtual environment if not exists
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+fi
 source ./.venv/bin/activate
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
+
 streamlit run app.py --server.port=8080
