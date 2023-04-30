9e8d13b01c6bf7300b0d6bbfe252df393f2f0891
Sid Sarasvati <2634859+sidsarasvati@users.noreply.github.com>
2023-04-30 17:28:18 -0400

    don't create venv

--- run_app.sh
+++ run_app.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
-# create virtual environment if not exists
-if [ ! -d ".venv" ]; then
-    echo "Creating virtual environment..."
-    python3 -m venv .venv
-fi
-. ./.venv/bin/activate
+
+# # create virtual environment if not exists
+# if [ ! -d ".venv" ]; then
+#     echo "Creating virtual environment..."
+#     python3 -m venv .venv
+# fi
+# . ./.venv/bin/activate
+
 pip install --no-cache-dir -r requirements.txt
 
 streamlit run app.py --server.port=8080
